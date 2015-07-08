720d2b0498aff0f0c44899ea3e584c6fa17ed4e0
Marc B <m.balaguerr@gmail.com>
2015-07-09 00:04:56 +0200

    Fixed face scale

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -15,14 +15,14 @@ class FaceView: UIView {
         var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay()}}
         var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay()}}
         
-        var scale = 0.90
+        var scale: CGFloat = 0.90
         
         var faceCenter: CGPoint {
             return convertPoint(center, fromView: superview)
         }
         
         var faceRadius: CGFloat {
-            return min(bounds.size.width, bounds.size.height) / 2
+            return min(bounds.size.width, bounds.size.height) / 2 * scale
         }
         
         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
