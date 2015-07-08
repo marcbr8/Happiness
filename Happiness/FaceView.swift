ee4f803896ca069c1b8853e4b63c00a9ce983546
Marc B <m.balaguerr@gmail.com>
2015-07-09 00:03:50 +0200

    Drawn initial face shape

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,36 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Marc Balaguer on 08/07/15.
+//  Copyright (c) 2015 Marc Balaguer. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func drawRect(rect: CGRect) {
+
+        var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay()}}
+        var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay()}}
+        
+        var scale = 0.90
+        
+        var faceCenter: CGPoint {
+            return convertPoint(center, fromView: superview)
+        }
+        
+        var faceRadius: CGFloat {
+            return min(bounds.size.width, bounds.size.height) / 2
+        }
+        
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+    }
+
+
+}
